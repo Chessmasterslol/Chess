cb7beaa6a3f8981ab4e9fed8fd8b8f20d770bd4f
Ageofempierswar <stoffe_data@hotmail.com>
2014-10-30 11:26:07 +0100

    Fixa något

--- Chess/Chessgame/Class1.cs
+++ Chess/Chessgame/Class1.cs
@@ -7,31 +7,32 @@ using System.Threading.Tasks;
 namespace Chessgame
 {
     public class Class1
-{
-            public Class1()
-            {
- 
- 
-                var Player1 = new Player();
-                var Player2 = new Player();
- 
-                    DrawoBoard(Player1, Player2);
- 
-                while (true)
-                {
- 
-                    var mymove = Player1.MakeMove(Player2.GetPieces());
- 
-            string[] board=  DrawoBoard(Player1, Player2);
-                    PrintBoard(board);
-                       
- 
-                    var mymove2 = Player2.MakeMove(Player1);
+    {
+        public Class1()
+        {
+
+
+            var Player1 = new Player();
+            var Player2 = new Player();
+
             DrawoBoard(Player1, Player2);
- 
- 
- 
-                }
- 
+
+            while (true)
+            {
+
+                var mymove = Player1.MakeMove(Player2.GetPieces());
+
+                string[] board = DrawoBoard(Player1, Player2);
+                PrintBoard(board);
+
+
+                var mymove2 = Player2.MakeMove(Player1);
+                DrawoBoard(Player1, Player2);
+
+
+
             }
+
+        }
+    }
 }
