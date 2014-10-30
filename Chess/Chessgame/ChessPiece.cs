97c24346777175403ac941457cb6d8a65d5f11a7
Ageofempierswar <stoffe_data@hotmail.com>
2014-10-30 11:18:27 +0100

    Flera ändringar

--- Chess/Chessgame/ChessPiece.cs
+++ Chess/Chessgame/ChessPiece.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Chessgame
 {
-     class ChessPiece : 
+     class ChessPiece  
     {
         king kung = new king();
         queen drottning = new queen();
--- Chess/Chessgame/Class1.cs
+++ Chess/Chessgame/Class1.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Chessgame
 {
-    class Class1
-    {
-    }
+    public class Class1
+{
+            public Class1()
+            {
+ 
+ 
+                var Player1 = new Player();
+                var Player2 = new Player();
+ 
+                    DrawoBoard(Player1, Player2);
+ 
+                while (true)
+                {
+ 
+                    var mymove = Player1.MakeMove(Player2.GetPieces());
+ 
+            string[] board=  DrawoBoard(Player1, Player2);
+                    PrintBoard(board);
+                       
+ 
+                    var mymove2 = Player2.MakeMove(Player1);
+            DrawoBoard(Player1, Player2);
+ 
+ 
+ 
+                }
+ 
+            }
 }
