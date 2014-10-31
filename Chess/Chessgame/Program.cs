2e3ce2891726a73472175d396deda3855cec921f
Ageofempierswar <stoffe_data@hotmail.com>
2014-10-31 12:32:54 +0100

    Addade lite kod

--- Chess/Chessgame/Program.cs
+++ Chess/Chessgame/Program.cs
@@ -8,13 +8,23 @@ namespace Chessgame
 {
     class Program
     {
+        public static String m_PlayerColor;
         static void Main(string[] args)
         {
 
             Chessbord chess = new Chessbord();
 
+            m_PlayerColor = "Black";
+            Console.WriteLine("Player color is...");
+            Console.WriteLine(m_PlayerColor);
+            if (m_PlayerColor == "Black")
 
+                Console.WriteLine("My first move is: e2 -> e4");
+            else
+                Console.WriteLine("Please enter your first move!");
         }
+        //choose the colour
+
         public enum ChessColor
         {
             White,
